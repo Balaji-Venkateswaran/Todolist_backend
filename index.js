218002b9f95b63bfe63efb9353b58f53c0e82ceb
Balaji <balaji.v@cannyfore.com>
2025-05-13 10:39:28 +0530

    update

--- index.js
+++ index.js
@@ -1,80 +1,4 @@
 
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const authRoutes = require("./routes/authRoutes");
-// const todoRoutes = require("./routes/todoRoutes");
-// const passwordRoutes = require("./routes/passwordRoutes");
-
-
-// const app = express();
-
-// app.use(bodyParser.json());
-// // app.use(cors());
-
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true,
-// }));
-
-// mongoose
-//   // .connect("mongodb://localhost:27017/todoapp", {
-//     // .connect("mongodb+srv://gamerjouer:Logan@2014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
-// .connect("mongodb+srv://gamerjouer:Logan%402014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
-
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.log(err));
-
-// app.use("/todos", todoRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/auth", passwordRoutes); 
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
-
-// // const express = require("express");
-// // const mongoose = require("mongoose");
-// // const bodyParser = require("body-parser");
-// // const cors = require("cors");
-// // const authRoutes = require("../routes/authRoutes");
-// // const todoRoutes = require("../routes/todoRoutes");
-// // const passwordRoutes = require("../routes/passwordRoutes");
-// // const serverless = require("serverless-http");
-
-// // const app = express();
-
-// // app.use(bodyParser.json());
-// // app.use(cors({
-// //   origin: "http://localhost:5173",
-// //   credentials: true,
-// // }));
-
-// // mongoose.connect("mongodb+srv://gamerjouer:Logan%402014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
-// //   useNewUrlParser: true,
-// //   useUnifiedTopology: true,
-// // })
-// //   .then(() => console.log("MongoDB connected"))
-// //   .catch((err) => console.log(err));
-
-// // app.use("/api/todos", todoRoutes);
-// // app.use("/api/auth", authRoutes);
-// // app.use("/api/password", passwordRoutes);
-
-// // // 👇 Export as a serverless function handler
-// // module.exports = serverless(app);
-
-
-
-const serverless = require("serverless-http");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -83,23 +7,37 @@ const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
 
+
 const app = express();
 
 app.use(bodyParser.json());
+// app.use(cors());
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
 
-mongoose.connect("mongodb+srv://gamerjouer:Logan%402014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  // .connect("mongodb://localhost:27017/todoapp", {
+    // .connect("mongodb+srv://gamerjouer:Logan@2014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
+    .connect("mongodb+srv://gamerjouer:Logan%402014@cluster.7agom4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster", {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/api/todos", todoRoutes);
+app.use("/todos", todoRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/password", passwordRoutes);
+app.use("/auth", passwordRoutes); 
 
-module.exports.handler = serverless(app);  
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
